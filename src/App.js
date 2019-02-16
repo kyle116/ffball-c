c2ba0fae318249c601203c8eee7ad3fbd9d626c4
Kyle Liu <kyle116@yahoo.com>
2019-02-15 20:13:03 -0800

    update file structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+    <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome</h1>
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/lobbies' component={LobbiesPage} />
       </div>
+    </Router>
     );
   }
 }
